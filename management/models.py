95a658a6b70bc925bb710f8a776d0d20e0d89fda
Bobby Robillard <robillard123@gmail.com>
2019-01-30 19:58:36 -0800

    Added Initial Models to Application

--- management/models.py
+++ management/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+name_length = 100
+decimal_digits = 2
+max_digits = 6
+
+class StandardBandit(models.Model):
+    name = models.CharField(max_length=name_length)
+    abbreviation = models.CharField(max_length=3)
+
+class Lense(models.Model):
+    zoom_height = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    zoom_diameter = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    focus_height = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    focus_diameter = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    make = models.CharField(max_length=name_length)
+    model = models.CharField(max_length=name_length)
+    custom_band_size = models.ForeignKey(StandardBandit, on_delete=models.CASCADE)
+
+class Setting(models.Model):
+    small_plunge_distance = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    medium_plunge_distance = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    large_plunge_distance = models.DecimalField(default=0, max_digits=max_digits, decimal_places=decimal_digits)
+    cnc_is_locked = models.BooleanField(default=False)
+    recent_orders_to_retain = models.PositiveIntegerField(default=0)
+
+class Manager(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- reporting/models.py
+++ reporting/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from management.models import Lense
+
+class RecentOrder(models.Model):
+    lenses = models.ManyToManyField(Lense)
+
+class Diagnostic(models.Model):
+    lense = models.ForeignKey(Lense, on_delete=models.CASCADE)
+    amount_processed = models.PositiveIntegerField(default=0)
