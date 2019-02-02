ad8a0b1f8a828e578836c42a1f5b718897a19fed
Bobby Robillard <robillard123@gmail.com>
2019-02-01 22:57:06 -0800

    Added system settings

--- management/urls.py
+++ management/urls.py
@@ -9,4 +9,5 @@ app_name = 'management'
 urlpatterns = [
     	# General Page Views
 		# url(r'^$', views.homepage_view, name='homepage'),
+		url(r'^settings$', views.settings_view, name='settings'),
 ]
