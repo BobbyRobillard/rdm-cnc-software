c28ed1a278e1ef2f952fa75505ea337ccde8a8f5
Bobby Robillard <robillard123@gmail.com>
2019-02-02 13:18:25 -0800

    Update main.js

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -1,21 +1,14 @@
-var admin_email = "bobby@rdminnovation.com";
-var system_administrator = "bobby@rdminnovation.com";
+var admin_email = "admin@techandmech.com";
 var csrftoken = getCookie('csrftoken');
-var app_failing_notice = "The application is not working, please contact " + system_administrator + ".";
-
-function app_failure() {
-  alert(app_failing_notice);
-}
-
-// using jQuery
+//------------------------------------------------------------------------------
 function getCookie(name) {
     var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
+    if (document.cookie && document.cookie != '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
@@ -23,31 +16,11 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-// -----------------------------------------------------------------------------
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-// -----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 function setup_ajax() {
   // Setup CSRF for AJAX
   $.ajaxSetup({
      beforeSend: function(xhr, settings) {
-         function getCookie(name) {
-             var cookieValue = null;
-             if (document.cookie && document.cookie != '') {
-                 var cookies = document.cookie.split(';');
-                 for (var i = 0; i < cookies.length; i++) {
-                     var cookie = jQuery.trim(cookies[i]);
-                     // Does this cookie string begin with the name we want?
-                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                         break;
-                     }
-                 }
-             }
-             return cookieValue;
-         }
          if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
              // Only send the token to relative URLs i.e. locally.
              xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
@@ -55,16 +28,8 @@ function setup_ajax() {
      }
   });
 }
-// -----------------------------------------------------------------------------
-function set_active_menu_tab(tab_id) {
-  $('.menu-item').each(function() {
-    $(this).css({
-      'background-color':'white',
-      'color':'var(--secondary-color)'
-    });
-  });
-  $('#' + tab_id).css({
-    'background-color':'var(--secondary-color)',
-    'color':'white'
-  });
+//------------------------------------------------------------------------------
+function direct_to_href(element) {
+  window.location.href = element.attr('href');
 }
+//------------------------------------------------------------------------------
